117943a7aebcf23d62e862c8e0016a8d39935821
Seokjong Park <psj216@gmail.com>
2020-03-26 23:01:35 +0900

    baekjoon 2748

---
+++ baekjoon/2748.py
@@ -0,0 +1,14 @@
+import sys
+N = int(sys.stdin.readline())
+cache = {1: 1, 2: 1}
+
+
+def fibonacci(n):
+    if n in cache:
+        return cache[n]
+    fibn = fibonacci(n-2) + fibonacci(n-1)
+    cache[n] = fibn
+    return fibn
+
+
+print(fibonacci(N))
